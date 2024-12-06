35841493a622fd8e38d480249bf20ae372819006
colinbrust <colin.brust@gmail.com>
2024-12-06 14:24:22 -0700

    Fastapi working in ln container.

--- loggernet/entrypoint.sh
+++ loggernet/entrypoint.sh
@@ -4,4 +4,4 @@ sudo dpkg --install /opt/ln/$LN_VERSION
 sudo ln -s /opt/CampbellSci/LoggerNet/cora_cmd /usr/local/bin/cora_cmd
 sudo /etc/init.d/csilgrnet start
 
-tail -f /dev/null
+/app/.venv/bin/fastapi run app/main.py --port 8000 --host 0.0.0.0
