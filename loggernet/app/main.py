5b6c6435dd859eedfe2dff9f95d9cbe97428b514
colinbrust <colin.brust@gmail.com>
2024-12-09 16:47:35 -0700

    Got debugger and compile checker working!

--- loggernet/app/main.py
+++ loggernet/app/main.py
@@ -12,20 +12,21 @@ async def root():
 @app.post("/compile")
 async def check_compile(file: UploadFile = File(...)):
     # Save the uploaded file temporarily
-    temp_file_path = f"/tmp/{file.filename}"
+    temp_file_path = f"/home/wine/{file.filename}"
     try:
         with open(temp_file_path, "wb") as temp_file:
             temp_file.write(await file.read())
         
         # Use subprocess to run the `check_compile` command
         try:
-            output = subprocess.check_output(["check_compile", temp_file_path], stderr=subprocess.STDOUT, text=True)
-            return JSONResponse(content={"message": "File checked successfully", "output": output}, status_code=200)
+            output = subprocess.check_output(["check_compile", temp_file_path], stderr=subprocess.STDOUT, text=True, cwd="/home/wine")
+            if "Failed" in output:
+                return JSONResponse(content={"message": "Compilation failed", "details": output}, status_code=400)
+            return JSONResponse(content={"message": "Compilation succeeded", "output": output}, status_code=200)
         except subprocess.CalledProcessError as e:
             # Handle errors from the command
             return JSONResponse(content={"error": "Compilation check failed", "details": e.output}, status_code=400)
     
     finally:
-        # Ensure the temporary file is deleted
         if os.path.exists(temp_file_path):
             os.remove(temp_file_path)
--- loggernet/entrypoint.sh
+++ loggernet/entrypoint.sh
@@ -4,11 +4,11 @@ sudo dpkg --install /opt/ln/$LN_VERSION
 sudo ln -s /opt/CampbellSci/LoggerNet/cora_cmd /usr/local/bin/cora_cmd
 sudo /etc/init.d/csilgrnet start
 
-uv sync --frozen --no-cache
+sudo uv sync --frozen --no-cache
 
 if [ "$APP_MODE" = "debug" ]; then
     echo "Debugging"
-    python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn main:app --host 0.0.0.0 --port 8000
+    /app/.venv/bin/python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m uvicorn app.main:app --host 0.0.0.0 --port 8080 --reload
 else
     echo "Not Debugging"
     /app/.venv/bin/fastapi run app/main.py --port 8080 --host 0.0.0.0 --reload
