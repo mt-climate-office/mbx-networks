9cf855d1a1f5e7249c66f3d1f03e5c1fa36ae6cd
colinbrust <colin.brust@gmail.com>
2024-12-16 16:23:36 -0700

    progress on program builder.

--- loggernet/app/instruments.py
+++ loggernet/app/instruments.py
@@ -1,13 +1,148 @@
-from abc import ABC, abstractmethod
-from pydantic import BaseModel
+from dataclasses import dataclass
+from textwrap import dedent
+import app.operators as op
 
-class Instrument(BaseModel):
+from typing import Literal
+
+@dataclass
+class Variable:
+    type: Literal["Public", "Const", "Dim", "Alias"]
     name: str
-    wiring_diagram: list[str] | None = None
-    variables: list[str]
-    tables: dict[str, list[str]]
-    pre_scan: list[str] | None = None
-    program: list[str] | None = None
-    slow_sequence: list[str] | None = None
+    value: str | int | float | None = None
+    units: str | None = None
 
-# define each sensor here
+    def __post_init__(self):
+        if self.type == "Const" and self.value is None:
+            raise ValueError("When defining a Const type, value must not be none.")
+    
+    def __str__(self):
+        out = f"{self.type} {self.name}"
+        if self.type == "Const":
+            out = f"{out} = {self.value}"
+
+        if self.units is not None:
+            out = f"{out} : Units {self.name} = {self.units}"
+
+        return out
+
+
+class TableItem:
+    def __init__(self, func: str, field_names: list[str], *args: str):
+        self.func = func
+        self.args = args
+        self.field_names = field_names
+
+    
+    def __str__(self):
+        return f"{self.func}({",".join(self.args)}) : FieldNames(\"{','.join(self.field_names)}\")"
+
+
+@dataclass
+class CardOut:
+    StopRing: Literal[0, 1] = 0
+    Size: int = -1
+
+
+@dataclass
+class DataInterval:
+    TIntoInt: int = 0
+    Interval: int = 5
+    Units: str = "min"
+    Lapses: int = 10
+
+
+class Table:
+    def __init__(self, name: str, *args: TableItem, trig_var: str=True, size: int=-1, data_interval: DataInterval = DataInterval(),
+                 card_out: CardOut | None=CardOut()):
+        self.name = name
+        self.trig_var = trig_var
+        self.size = size
+        self.args = args
+        self.data_interval = data_interval
+        self.card_out = card_out
+
+    def __str__(self):
+        s = f"DataTable({self.name},self.trig_var)\n"
+        s += f"    {str(self.data_interval)}\n"
+        if self.card_out:
+            s += f"    {str(self.card_out)}\n"
+        
+        for item in self.args:
+            s += f"\n{str(item)}"
+
+@dataclass
+class Wire:
+    wire: str
+    port: str
+    description: str | None
+
+    def __str__(self):
+        return f"{self.wire + ':':<8} {self.port:<8} {self.description:<10}"
+
+
+class WiringDiagram:
+
+    def __init__(self, *args: Wire, description: str | None = None):
+        self.args = args
+        self.description = description
+
+    def __str__(self):
+        out = "'" + "\n'".join(str(x) for x in self.args)
+        if self.description is not None:
+            out += f"\n'{self.description}\n"
+        return out
+
+
+@dataclass
+class Instrument:
+    manufacturer: str
+    model: str
+    type: str
+    elevation: int | None = None
+    sdi12_address: str | None = None
+    wiring: str | None = None
+    variables: list[Variable] | None = None
+    tables: list[Table] | None = None
+    pre_scan: str | None = None
+    program: str | None = None
+    slow_sequence: str | None = None
+
+    def __post_init__(self):
+        self._extend_variable_names()
+        self._validate_variables()
+
+
+    def _extend_variable_names(self):
+        for v in self.variables:
+            if self.elevation is not None:
+                v.name += f"_{self.elevation}"
+            if self.sdi12_address is not None:
+                v.name += f"_id{self.sdi12_address}"
+    
+    def _validate_variables(self):
+        # TODO: This method will check that all variablese in 
+        # the tables are defined as variables.
+        ...
+
+
+RMYoung_05108_74 = Instrument(
+    manufacturer = "RM Young",
+    model = " 05108-74",
+    type = "Wind",
+    elevation=1000,
+    wiring = WiringDiagram(
+        Wire("Red", "P1", "WS Signal      WS SIG"),
+        Wire("White", "VX2", "WD Excite      WD EXC"),
+        Wire("Green", "SE7", "WD Signal      WD SIG"),
+        Wire("Black", "AG", "Signal G       WD REF"),
+        Wire("Brown", "G", "Earth G        GND*"),
+        description = "* NOTE: Ground to EARTH in junction box directly to mast"
+    ),
+    variables = list(
+        Variable("Const", "WS_offset", 0),
+        Variable("Const", "WS_multiplier", 0.1666),
+        Variable("Public", "wind_spd", units="m s-1"),
+        Variable("Public", "wind_dir", units="arcdeg"),
+        Variable("Public", "wind_timer", units="sec")
+    )
+)
--- loggernet/app/program.py
+++ loggernet/app/program.py
@@ -1,67 +1,26 @@
-from __future__ import annotations
+from pydantic import BaseModel
+from datetime import date
 
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from instruments import Instrument
+from typing import Literal
 
+class Program(BaseModel):
+    name: str
+    instruments: list[Instrument]
+    mode: Literal["SequentialMode", "PipelineMode"] = "SequentialMode"
+    preserve_variables: bool = True
 
-class Container(ABC):
+    def construct(self):
+        s = f"'{self.name}\n'Program Created on: {date.today()}\n\n"
+        s += "'SYSTEM CONFIGURATION\n"
+        s += "\n".join(f"'{x.type}: {x.manufacturer} {x.model}" for x in self.instruments)
+        s += "\n\nWiring Diagram\n"
+        for i in self.instruments:
+            s += f"####{i.model} Wiring####\n"
+            s += str(i.wiring)
+        
+        
 
-    def __init__(self, *args, children: list[Container] | list[str], indent: int=0, **kwargs):
-        self.args = args
-        self.children = children
-        self.indent = indent
-        self.kwargs = kwargs
+        s += "\n".join()
 
-    @abstractmethod
-    def __str__(self) -> str:
         ...
-
-@dataclass
-class program(Container):
-
-    def to_str(self):
-        return f"""
-
-        """
-    
-@dataclass
-class If(Container):
-    
-    def __init__(self):
-        self.initial_condition = self._join_if_logic(self.args)
-        self.else_ifs = []
-        self.else_logic = None
-
-    def _join_if_logic(self, *args, is_initial: bool=True):
-        if is_initial:
-            return f"If {' '.join(args)} Then"
-        return f"ElseIf {' '.join(args)} Then"
-    
-    def ElseIf(self, *args, logic):
-        self.else_ifs.append(
-            f"""
-            {self._join_if_logic(*args, False)}
-                {logic}
-            """
-        )
-    
-    def Else(self, logic):
-        self.else_logic = f"Else\n    {logic}"
-        return self
-    
-    def __str__(self):
-
-        code = [self.initial_condition]
-        code.append(f"    {self.logic}")
-
-        for elseif in self.else_ifs:
-            code.append(elseif)
-
-        if self.else_logic:
-            code.append(self.else_logic)
-
-        code.append("EndIf")
-        return "\n".join(code)
-    
-    def Else
-
